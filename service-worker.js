0a429c3af69f93a1e5c7b1173e99d0c13225a590
business <89967850+leaderabdelaly@users.noreply.github.com>
2026-01-02 04:26:59 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,21 @@
+const CACHE = "shehaby-shooting-pro-v1";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("shooting-pro").then(cache => {
-      return cache.addAll([
+    caches.open(CACHE).then(c =>
+      c.addAll([
         "./",
         "./index.html",
         "./style.css",
         "./app.js",
         "./lang.json"
-      ]);
-    })
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
